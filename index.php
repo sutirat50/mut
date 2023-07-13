327e4f1c362919a9801d4a8d720f421e2e1b5b2c
KM <6411130050@mut.ac.th>
2023-07-13 14:36:53 +0700

    230712

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php echo "my repository"; ?>
+ <?php echo "yarkyai"; ?>
+
+ <?php echo "Computer Engineering"; ?>
+ <?php echo "Mahanakorn"; ?>
+ <?php echo "I love Doraemon"; ?>
+ <?php echo "I love Doraemon"; ?>
